f8695888d2d03d6482ccad4e6ff9284e84eccc67
Surote Wongpaiboon <surote.z9@gmail.com>
2023-02-28 21:48:08 +0700

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask,request
+
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+@app.route('/')
+def home():
+    re_dict = {'headers': {},'remote_addr': str(request.remote_addr)}
+
+    print(request.headers, request.json,request.query_string,request.remote_addr)
+    for i in request.headers:
+        if i[0] not in re_dict.keys():
+            re_dict['headers'][i[0]] = i[1]
+    return re_dict
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5124)
