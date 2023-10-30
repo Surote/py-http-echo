04b84150ce36a4b9fae9d99d8c52362f37b8497c
Surote Wongpaiboon <swongpai@redhat.com>
2023-10-30 10:32:58 +0700

    add api/v1

--- app.py
+++ app.py
@@ -18,5 +18,15 @@ def home():
     return render_template('home.html', json_data=json.dumps(re_dict, indent=4), background_color=background_color)
     #return re_dict
 
+@app.route('/api/v1')
+def json_return():
+    re_dict = {'headers': {},'remote_addr': str(request.remote_addr),'pod_name': str(os.getenv("HOSTNAME","N/A"))}
+    print(request.headers, request.json,request.query_string,request.remote_addr)
+    for i in request.headers:
+        if i[0] not in re_dict.keys():
+            re_dict['headers'][i[0]] = i[1]
+
+    return re_dict
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8080)
