3f7a123f2d238f84634e1f93982dcdf659c833d7
Surote Wongpaiboon <swongpai@redhat.com>
2023-10-30 10:20:09 +0700

    add html template and bg os env

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ WORKDIR /py-http-echo
 COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
 
+COPY templates ./templates/
 COPY app.py .
 
 CMD [ "python3", "app.py"]
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask,request
+from flask import Flask,request,render_template
 import os
+import json
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
@@ -7,12 +8,15 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 @app.route('/')
 def home():
     re_dict = {'headers': {},'remote_addr': str(request.remote_addr),'pod_name': str(os.getenv("HOSTNAME","N/A"))}
-
+    background_color = os.environ.get("BACKGROUND_COLOR", "blue")
     print(request.headers, request.json,request.query_string,request.remote_addr)
     for i in request.headers:
         if i[0] not in re_dict.keys():
             re_dict['headers'][i[0]] = i[1]
-    return re_dict
+
+    # Render an HTML template with the background color
+    return render_template('home.html', json_data=json.dumps(re_dict, indent=4), background_color=background_color)
+    #return re_dict
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8080)
